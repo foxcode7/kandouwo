97d574cdac471aecbe2c8c030d80f8b4d4444a06
杨鑫 <yangxin03@meituan.com>
2014-11-07 15:46:47 +0800

    主页加了我的书架和导入藏书tab，IDE又掉链子了

--- app/src/main/java/com/kindleren/kandouwo/home/HomeFragment.java
+++ app/src/main/java/com/kindleren/kandouwo/home/HomeFragment.java
@@ -1,5 +1,9 @@
 package com.kindleren.kandouwo.home;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -7,11 +11,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.support.v4.view.ViewPager;
 
 import com.google.inject.Inject;
 import com.kindleren.kandouwo.R;
 import com.kindleren.kandouwo.base.BaseFragment;
+import com.kindleren.kandouwo.common.views.PagerSlidingTabStrip;
 import com.kindleren.kandouwo.search.SearchBookKeywordsActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by foxcoder on 14-9-22.
@@ -21,6 +30,9 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
     @Inject
     private LayoutInflater inflater;
 
+    private ViewPager viewPager;
+    private PagerSlidingTabStrip pstHome;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,7 +40,18 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        viewPager = (ViewPager) view.findViewById(R.id.pager);
+        pstHome = (PagerSlidingTabStrip) view.findViewById(R.id.pst_home);
+
+        List<TabInfo> list = new ArrayList<TabInfo>();
+        list.add(new TabInfo(MyShelfFragment.class, null, "我的书架"));
+        list.add(new TabInfo(LocalShelfFragment.class, null, "导入藏书"));
+
+        HomeFragmentPagerAdapter adapter = new HomeFragmentPagerAdapter(getActivity(), getChildFragmentManager(), list);
+        viewPager.setAdapter(adapter);
+        pstHome.setViewPager(viewPager);
+        return view;
     }
 
     @Override
@@ -47,6 +70,48 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
             intent.setClass(getActivity(), SearchBookKeywordsActivity.class);
             startActivity(intent);
             return;
+        }
+    }
+
+    private static class HomeFragmentPagerAdapter extends FragmentPagerAdapter {
+        private Context context;
+        private List<TabInfo> list;
+
+        public HomeFragmentPagerAdapter(Context c, FragmentManager fm, List<TabInfo> l) {
+            super(fm);
+            context = c;
+            list = l;
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            TabInfo info = list.get(position);
+            return Fragment.instantiate(context, info.clss.getName(), info.bundle);
+        }
+
+        @Override
+        public int getCount() {
+            if (list == null)
+                return 0;
+            return list.size();
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return list.get(position).title;
+        }
+    }
+
+
+    private static class TabInfo {
+        Class<?> clss;
+        Bundle bundle;
+        String title;
+
+        public TabInfo(Class<?> c, Bundle b, String s) {
+            clss = c;
+            bundle = b;
+            title = s;
         }
     }
 }
---
+++ app/src/main/java/com/kindleren/kandouwo/home/MyShelfFragment.java
@@ -0,0 +1,25 @@
+package com.kindleren.kandouwo.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.inject.Inject;
+
+import com.kindleren.kandouwo.R;
+import com.kindleren.kandouwo.base.BaseFragment;
+
+/**
+ * Created by foxcoder on 14-11-7.
+ */
+public class MyShelfFragment extends BaseFragment {
+
+    @Inject
+    private LayoutInflater inflater;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_my_shelf, container, false);
+    }
+}
